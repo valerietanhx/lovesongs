3dde68e4bfc4ec660c613fd6aacefe5fa0f1367a
Valerie Tan <valerietanhx@gmail.com>
2024-05-19 20:44:23 +0800

    update dummy data

--- database/insert_dummy_data.sql
+++ database/insert_dummy_data.sql
@@ -21,4 +21,32 @@ values
         'happy new year',
         'sitting in the car during SEP roadtrips...',
         now()
+    ),
+    (
+        'vale',
+        'loona',
+        'eclipse',
+        'rmb listening to this as part of a big loona solos playlist back in may? june? 2018... the beginning of my journey with them! :)',
+        now()
+    ),
+    (
+        'vale',
+        'got7',
+        'eclipse',
+        'loona covered this back in late 2019 and i listened to it a lot... can basically play the video in my head. this reminds me of the gsp japan trip!',
+        now()
+    ),
+    (
+        'vale',
+        'phoebe bridgers',
+        'chinese satellite',
+        'june 2021 waiting for uni to start... golden hour sunlight pooling on my table during virtual ori camps :,)',
+        now()
+    ),
+    (
+        'vale',
+        'lorde',
+        'buzzcut season',
+        'idr when i started getting into lorde, but it was during the december holidays i believe, in 2015 or 2016. had just come back from sending my helper off at the airport; listened to the pure heroine album the entire time back home, then lied on the sofa listening more... it was a calm and cloudy morning.',
+        now()
     );
